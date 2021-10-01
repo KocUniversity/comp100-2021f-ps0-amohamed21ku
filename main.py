399be875714a20a26adad43631d06cfb70de90a9
ABDELRAHMAN SAMIR SAAD HASSAN SAMIR <91738853+amohamed21ku@users.noreply.github.com>
2021-10-01 13:49:49 +0000

    Done with the first assignment

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hi")
+import math
+x=int(input("Enter number x : "))
+y=int(input("Enter number y : "))
+result= (x**y)
+print("x**y =", result)
+print("log(x) = ",math.log2(x)  )
+print("0080848")
